168156b209dde3d27f3fc6bb0bfb20fe4b0fbda1
Spencer_Durfey <durfey_32@yahoo.com>
2024-09-04 21:07:37 -0600

    added the index.ts and .gitignore and connection.ts

--- db/schema.sql
+++ db/schema.sql
@@ -5,23 +5,23 @@ CREATE DATABASE employee_tracker_db;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL(10, 2),
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO departments (name) 
-VALUES ('HR', 'Accounting', 'Customer Service', 'Legal"', 'IT');
+VALUES ('HR'), ('Accounting'), ('Customer Service'), ('Legal'), ('IT'), ('Sales');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES ('HR Manager', 100000, 1),
@@ -10,13 +10,20 @@ VALUES ('HR Manager', 100000, 1),
        ('Lawyer', 100000, 4),
        ('IT Manager', 80000, 5),
        ('IT Specialist', 60000, 5);
+       ('Sales Manager', 90000, 6),
+       ('Sales Associate', 50000, 6);
+       ('CEO', 200000, NULL);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1, NULL),
+VALUES ('John', 'Doe', 1, 11),
        ('Jane', 'Smith', 2, 1),
-       ('Mike', 'Jones', 3, 2),
-       ('Sarah', 'Miller', 4, 3),
-       ('Chris', 'Brown', 5, 4),
-       ('Jessica', 'Davis', 6, 5),
-       ('David', 'Moore', 7, 6),
+       ('Mike', 'Jones', 3, 11),
+       ('Sarah', 'Miller', 4, 1),
+       ('Chris', 'Brown', 5, 6),
+       ('Jessica', 'Davis', 6, 11),
+       ('David', 'Moore', 7, 11),
        ('Ashley', 'Taylor', 8, 7);
+       ('Robert', 'Wilson', 9, 11),
+       ('Emily', 'Johnson', 10, 9);
+       ('James', 'White', 10, 9);
+       ('Jill', 'Green', 11, NULL);
